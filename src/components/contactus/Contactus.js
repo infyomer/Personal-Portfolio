2edeba1ea1872c29fe7e1d101a1c4a1717cfeffa
Aman Kumar Sharma <amankumarsharma01234@gmail.com>
2024-12-20 11:39:17 +0530

    Initial Commit

--- src/components/contactus/Contactus.js
+++ src/components/contactus/Contactus.js
@@ -8,37 +8,6 @@ import { FiPhone, FiAtSign } from "react-icons/fi";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 
 export default function Contactus() {
-  const [formData, setFormData] = useState(new FormData());
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!(formData.name && formData.email && formData.message)) {
-      alert("Something went wrong!");
-      return;
-    }
-
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/api/submitForm",
-        formData
-      );
-      console.log(response.data.message); // Log the response from the backend
-
-      alert(`Thanks ${formData.name}, I will shortly connect with you!`);
-    } catch (error) {
-      console.error("Error submitting the form:", error);
-
-      alert("Backend Server was not Running while submitting the form.");
-    }
-
-    setFormData({});
-  };
-
   return (
     <div>
       <Container fluid className="certificate-section" id="about">
@@ -54,67 +23,6 @@ export default function Contactus() {
             </Col>
             <Col md={12} id="contact" className="mt-3">
               <Row>
-                <Col md={4}>
-                  <div className="contacts-form" data-aos="fade-up">
-                    <form>
-                      <div className="input-container d-flex flex-column">
-                        <label htmlFor="username" className="label-class">
-                          Full Name
-                        </label>
-                        <input
-                          type="text"
-                          className="form-input input-class"
-                          id="username"
-                          name="name"
-                          aria-describedby="emailHelp"
-                          placeholder="Enter your name"
-                          value={formData.name || ""}
-                          onChange={handleChange}
-                        />
-                      </div>
-                      <div className="input-container d-flex flex-column">
-                        <label htmlFor="email" className="label-class">
-                          Email address
-                        </label>
-                        <input
-                          type="email"
-                          className="form-input input-class"
-                          name="email"
-                          id="email"
-                          aria-describedby="emailHelp"
-                          placeholder="Enter email"
-                          value={formData.email || ""}
-                          onChange={handleChange}
-                        />
-                      </div>
-                      <div className="input-container d-flex flex-column">
-                        <label htmlFor="userMessage" className="label-class">
-                          Message
-                        </label>
-                        <textarea
-                          className="form-message input-class"
-                          id="userMessage"
-                          name="message"
-                          rows="3"
-                          placeholder="Enter message"
-                          value={formData.message || ""}
-                          onChange={handleChange}
-                        />
-                      </div>
-
-                      <div className="submit-btn">
-                        <button
-                          type="submit"
-                          className="submitBtn"
-                          onClick={handleSubmit}
-                        >
-                          Submit
-                          <AiOutlineSend className="send-icon" />
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                </Col>
                 <Col md={7}>
                   <div className="contacts-details">
                     <a
@@ -150,8 +58,7 @@ export default function Contactus() {
                         </p>
                       </div>
                     </a>
-                  </div>
-                 
+                  </div>                 
                 </Col>
               </Row>
             </Col>
@@ -161,3 +68,4 @@ export default function Contactus() {
     </div>
   );
 }
+
